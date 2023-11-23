eb120850a881aa7760e8bdc399c05bbc328a11e4
Aleksandra <a.haniok@gmail.com>
2023-11-23 19:25:51 +0000

    Clean Up Data Fetching - cancel current request each time a new one comes in

--- src/App.js
+++ src/App.js
@@ -63,12 +63,15 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError('');
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${apiKey}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${apiKey}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok)
@@ -77,9 +80,12 @@ export default function App() {
           const data = await res.json();
           if (data.Response === 'False') throw new Error('Movie not found');
           setMovies(data.Search);
+          setError('');
         } catch (err) {
           console.error(err);
-          setError(err.message);
+          if (err.name !== 'AbortError') {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -91,6 +97,10 @@ export default function App() {
       }
 
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
